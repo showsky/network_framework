289329ae0936f798c35e6dbc7f58678d2d0b6177
Ting Cheng <showsky@gmail.com>
2015-04-16 10:57:12 +0800

    1. Improve ApiException code

--- src/main/java/com/miiicasa/casa/exception/ApiException.java
+++ src/main/java/com/miiicasa/casa/exception/ApiException.java
@@ -6,14 +6,47 @@ package com.miiicasa.casa.exception;
 public class ApiException extends Exception {
 
     private final static String TAG = ApiException.class.getSimpleName();
-    private int errno = 0;
+    public enum TYPE {
+        API_ERROR,
+        JSON_FORMAT_ERROR,
+    };
+    private static String[] message = {
+        "API error",
+        "JSON format error",
+    };
+    private TYPE apiType = null;
+    private int errNo = 0;
+    private String errMessage;
 
-    public ApiException(int errno) {
-        super("Api fail errno: " + errno);
-        this.errno = errno;
+    public ApiException(int errNo) {
+        super("Api fail errno: " + errNo);
+        this.errNo = errNo;
+        this.errMessage = "Api fail errno: " + errNo;
+        this.apiType = TYPE.API_ERROR;
+    }
+
+    public ApiException(int errNo, String errMessage) {
+        super("Api fail errno: " + errNo + " errMessage: " + errMessage);
+        this.errNo = errNo;
+        this.errMessage = errMessage;
+        this.apiType = TYPE.API_ERROR;
+    }
+
+    public ApiException(TYPE apiType) {
+        super(message[apiType.ordinal()]);
+        this.errMessage = message[apiType.ordinal()];
+        this.apiType = apiType;
     }
 
     public int getErrNo() {
-        return errno;
+        return errNo;
+    }
+
+    public String getErrMessage() {
+        return errMessage;
+    }
+
+    public TYPE getApiType() {
+        return apiType;
     }
 }
